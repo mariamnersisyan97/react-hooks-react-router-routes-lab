14c852f77cc03768ce4fa4e6671032f6967d6dcf
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-05-19 19:56:08 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -20,6 +20,5 @@ function Actors() {
     </div>
   );
 }
-//This component should render the text Actors Page in an <h1>, and make a new <div> for each actor. The <div> should contain the actor's name and a <ul> with a list of their movies.
 
 export default Actors;
