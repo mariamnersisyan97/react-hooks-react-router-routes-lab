8b605bf1f6112bebcd1ae4f0bde0c3803b75bec1
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-05-19 19:54:54 -0400

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { Switch } from "react-router-dom";
 import { NavLink } from "react-router-dom";
+import Actors from "./Actors";
+import Directors from "./Directors";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/movies">Movies</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
