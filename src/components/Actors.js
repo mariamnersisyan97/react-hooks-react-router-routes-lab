b758762b162811e95824e9a6dda6452f4774bbe3
Mariam Nersisyan <mariamnersisyan97@gmail.com>
2022-05-19 19:54:51 -0400

    commit tree a84bd4f2b2ff2cc6f17e773e8cef3d492d89450c

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorsList = actors.map((actor) => (
+    <div key={actor.name}>
+      <h1>Name: {actor.name}</h1>
+      <p>Movies: </p>
+      <ul>
+        {actor.movies.map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actorsList}
+    </div>
+  );
 }
+//This component should render the text Actors Page in an <h1>, and make a new <div> for each actor. The <div> should contain the actor's name and a <ul> with a list of their movies.
 
 export default Actors;
